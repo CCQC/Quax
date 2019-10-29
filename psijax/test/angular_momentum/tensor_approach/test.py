45b85eb1a8a9941db6ad88c6221608a240f28c46
adabbott <adabbott@uga.edu>
2019-10-29 15:06:01 -0400

    dd block overlap integrals implemented. probably bugs with shapes

--- psijax/test/angular_momentum/tensor_approach/test.py
+++ psijax/test/angular_momentum/tensor_approach/test.py
@@ -26,27 +26,100 @@ def overlap_ss(A, B, alpha_bra, alpha_ket):
 
 @jax.jit
 def overlap_ps_block(A, B, alpha_bra, alpha_ket):
-    oot_alpha = 1 / (2 * alpha_bra)
-    return oot_alpha * jax.jacrev(overlap_ss,0)(A,B,alpha_bra,alpha_ket)
+    oot_alpha_bra = 1 / (2 * alpha_bra)
+    return oot_alpha_bra * jax.jacrev(overlap_ss,0)(A,B,alpha_bra,alpha_ket)
 
 @jax.jit
-def overlap_sp_block(A, B, alpha_bra, alpha_ket): # not really needed
-    oot_alpha = 1 / (2 * alpha_bra)
-    return oot_alpha * jax.jacrev(overlap_ss,1)(A,B,alpha_bra,alpha_ket)
+def overlap_sp_block(A, B, alpha_bra, alpha_ket): # not really needed is it?
+    oot_alpha_bra = 1 / (2 * alpha_bra)
+    return oot_alpha_bra * jax.jacrev(overlap_ss,1)(A,B,alpha_bra,alpha_ket)
 
 @jax.jit
 def overlap_pp_block(A, B, alpha_bra, alpha_ket):
-    oot_alpha_bra = 1 / (2 * alpha_bra)
+    # We are promoting the ket, so the factor is the ket exponent
     oot_alpha_ket = 1 / (2 * alpha_ket)
     # No second term, ai is 0 since we are promoting the ket and theres no AM in the ket.
     return oot_alpha_ket * (jax.jacfwd(overlap_ps_block, 1)(A,B,alpha_bra,alpha_ket))
 
+#@jax.jit
+#def overlap_ds_block(A,B,alpha_bra,alpha_ket):
+#    # We are promoting the bra a second time, factor is bra exponent
+#    oot_alpha_bra = 1 / (2 * alpha_bra)
+#    #                      # This is of shape (3,3) all dij combos symmetric matrix    # Thus a_i factor has to be 3x3 identity, so that only 
+#    return oot_alpha_bra * (jax.jacfwd(overlap_ps_block, 0)(A,B,alpha_bra,alpha_ket) + np.eye(3) * overlap_ss(A,B,alpha_bra,alpha_ket))
+
+@jax.jit
 def overlap_ds_block(A,B,alpha_bra,alpha_ket):
-    oot_alpha = 1 / (2 * alpha_bra)
+    '''
+    Returns a 1x6 array:
+    (dxx,s) (dxy,s)  (dxz,s) (dyy,s) (dyz,s) (dzz,s) 
+    '''
+    # We are promoting the bra a second time, factor is bra exponent
+    oot_alpha_bra = 1 / (2 * alpha_bra)
+    #                      # This is of shape (3,3) all dij combos symmetric matrix    # Thus a_i factor has to be 3x3 identity, so that only 
+    result = oot_alpha_bra * (jax.jacfwd(overlap_ps_block, 0)(A,B,alpha_bra,alpha_ket) + np.eye(3) * overlap_ss(A,B,alpha_bra,alpha_ket))  
+    # This result is a 3x3 array containing all (dxx,s) (dxy,s) (dyx,s), only need upper or lower triangle
+    # Return lower triangle ((dxx, dxy, dxz, dyy, dyz, dzz) | s) as a vector
+    il = np.tril_indices(3)
+    return result[il]
+
+@jax.jit
+def overlap_dp_block(A,B,alpha_bra,alpha_ket): 
+    '''
+    Returns a 1x18 array:
+    (dxx,px) (dxx,py) (dxx,pz) (dxy,px) (dxy,py) (dxy,pz) (dxz,px) (dxz,py) (dxz,pz) (dyy,px) (dyy,py) (dyy,pz) (dyz,px) (dyz,py) (dyz,pz) (dzz,px) (dzz,py) (dzz,pz)
+    If called directly, should reshape into a 6x3 block!
+    (dxx,px) (dxx,py) (dxx,pz) 
+    (dxy,px) (dxy,py) (dxy,pz) 
+    (dxz,px) (dxz,py) (dxz,pz) 
+    (dyy,px) (dyy,py) (dyy,pz) 
+    (dyz,px) (dyz,py) (dyz,pz) 
+    (dzz,px) (dzz,py) (dzz,pz)
+    '''
+    oot_alpha_ket = 1 / (2 * alpha_ket) # use ket, since we are promoting ket from s-->p
+    # This is a 18x1 array of d by p functions. Could also use overlap_pp_block instead, i think? 
+    return np.ravel(oot_alpha_ket * jax.jacfwd(overlap_ds_block, 1)(A,B,alpha_bra,alpha_ket))
+
+def overlap_dd_block(A,B,alpha_bra,alpha_ket): 
+    '''
+    Returns a 6x6 array:
+    (dxx,dxx) (dxx,dxy) (dxx,dxz) (dxx,dyy) (dxx,dyz) (dxx,dzz)
+    (dxy,dxx) (dxy,dxy) (dxy,dxz) (dxy,dyy) (dxy,dyz) (dxy,dzz)
+    (dxz,dxx) (dxz,dxy) (dxz,dxz) (dxz,dyy) (dxz,dyz) (dxz,dzz)
+    (dyy,dxx) (dyy,dxy) (dyy,dxz) (dyy,dyy) (dyy,dyz) (dyy,dzz)
+    (dyz,dxx) (dyz,dxy) (dyz,dxz) (dyz,dyy) (dyz,dyz) (dyz,dzz)
+    (dzz,dxx) (dzz,dxy) (dzz,dxz) (dzz,dyy) (dzz,dyz) (dzz,dzz)
+    '''
+    oot_alpha_ket = 1 / (2 * alpha_ket) # use ket, since we are promoting ket from p-->d
+    # The jacfwd (first) term is an 18x3 array           # ai coeffs are   # the second term is
+    # (dxx,px) --> (dxx,dxx) (dxx, dxy), (dxx, dxz)      1, 0, 0           (dxx|s) (dxx|s) (dxx|s)
+    # (dxx,py) --> (dxx,dyx) (dxx, dyy), (dxx, dyz)      0, 1, 0           (dxx|s) (dxx|s) (dxx|s)
+    # (dxx,pz) --> (dxx,dzx) (dxx, dzy), (dxx, dzz)      0, 0, 1           (dxx|s) (dxx|s) (dxx|s)
+    # (dxy,px) --> (dxy,dxx) (dxy, dxy), (dxy, dxz)      1, 0, 0           (dxy|s) (dxy|s) (dxy|s)
+    # (dxy,py) --> (dxy,dyx) (dxy, dyy), (dxy, dyz)      0, 1, 0           (dxy|s) (dxy|s) (dxy|s)
+    # (dxy,pz) --> (dxy,dzx) (dxy, dzy), (dxy, dzz)      0, 0, 1           (dxy|s) (dxy|s) (dxy|s)
+    # ....                                               ...              
+    # (dzz,px) --> (dzz,dxx) (dzz, dxy), (dzz, dxz)      1, 0, 0           (dzz|s) (dzz|s) (dzz|s)
+    # (dzz,py) --> (dzz,dyx) (dzz, dyy), (dzz, dyz)      0, 1, 0           (dzz|s) (dzz|s) (dzz|s)
+    # (dzz,pz) --> (dzz,dzx) (dzz, dzy), (dzz, dzz)      0, 0, 1           (dzz|s) (dzz|s) (dzz|s)
+    first_term = jax.jacfwd(overlap_dp_block, 1)(A,B,alpha_bra,alpha_ket)
+    factor = np.tile(np.eye(3),(6,1))
+    tmp_second_term = overlap_ds_block(A,B,alpha_bra,alpha_ket)
+    second_term = factor * np.repeat(tmp_second_term, 9).reshape(18,3)
+
+    result = oot_alpha_ket * (first_term + second_term)
+    # result is of same signature as jacfwd (first) term above
+    # It contains duplicates in each 3x3 sub-array (upper and lower triangle are equal)
+    # reshape and grab out just lower triangle as a vector, reshape into matrix
+    il1,il2 = np.tril_indices(3)
+    result = result.reshape(6,3,3)[:,il1,il2].reshape(6,6)
+    return result
     
-    overlap_ps_block, 0)
+ 
+
      
 
+#d_dB_overlap_ps
 
 
 geom = np.array([[0.0,0.0,-0.849220457955],
@@ -63,63 +136,12 @@ print(overlap_ps_block(A,B,alpha_bra,alpha_ket))
 print('hard coded')
 print(overlap_pp_block(A,B,alpha_bra,alpha_ket))
 
-for i in range(1000):
-    overlap_pp_block(A,B,alpha_bra,alpha_ket)
+print('hard coded')
+print(overlap_ds_block(A,B,alpha_bra,alpha_ket))
 
-#print(overlap_pp_block(A,B,alpha_bra,alpha_ket))
-#print(overlap_pp_block(A,B,alpha_bra,alpha_ket))
-#print(overlap_pp_block(A,B,alpha_bra,alpha_ket))
-#print(overlap_pp_block(A,B,alpha_bra,alpha_ket))
-#print(overlap_pp_block(A,B,alpha_bra,alpha_ket))
-#print(overlap_pp_block(A,B,alpha_bra,alpha_ket))
-#print(overlap_pp_block(A,B,alpha_bra,alpha_ket))
-#print(overlap_pp_block(A,B,alpha_bra,alpha_ket))
-#print(overlap_pp_block(A,B,alpha_bra,alpha_ket))
-#
 
-## S S test
-#exponents = np.repeat(0.5, 2)
-#nbf_per_atom = np.array([1,1])
-#angular_momentum = np.array([[0,0,0], [0,0,0]])
+overlap_dp_block(A,B,alpha_bra,alpha_ket)
 
-## S P test
-#exponents = np.repeat(0.5, 4)
-#nbf_per_atom = np.array([1,3])
-#angular_momentum = np.array([[0,0,0], [1,0,0], [0,1,0], [0,0,1]])
-
-## P P test 
-#exponents = np.repeat(0.5, 6)
-#nbf_per_atom = np.array([3,3])
-#angular_momentum = np.array([[1,0,0], [0,1,0], [0,0,1], [1,0,0],[0,1,0],[0,0,1]])
-
-## S P D test
-#exponents = np.repeat(0.5, 10)
-#nbf_per_atom = np.array([4,6])
-#angular_momentum =  np.array([[0,0,0], [1,0,0], [0,1,0], [0,0,1], [2,0,0], [1,1,0], [1,0,1], [0,2,0], [0,1,1], [0,0,2]])
-
-#centers = np.repeat(geom, nbf_per_atom, axis=0)
-#nbf = exponents.shape[0]
-#
-## Preprocess. Get unique oei indices, their 6d angular momentum vectors, and the indices which need differentiating
-## Separate integrals into classes
-## For every one electron integral, determine its integral class (s|s) (p|s) (p|p) etc
-#unique_oei_indices = []
-#oei_angular_momentum = []
-#integral_class = []
-#grad_indices = []
-#for i in range(nbf):
-#    for j in range(i+1):
-#        unique_oei_indices.append([i,j])
-#        pi, pj, pk = angular_momentum[i]
-#        qi, qj, qk = angular_momentum[j]
-#        target_am = onp.array([pi,pj,pk,qi,qj,qk])
-#        oei_angular_momentum.append(target_am)
-#        #integral_class.append([onp.sum(angular_momentum[i]), onp.sum(angular_momentum[j])])
-#        bra = onp.sum(angular_momentum[i])
-#        ket = onp.sum(angular_momentum[i])
-
-#        if bra == ket == 0:
-#
-#        if (bra == 1 and ket == 0) or (bra == 0 and ket == 1):
-#
-#        if (bra == 1 and ket == 1): 
+overlap_dd_block(A,B,alpha_bra,alpha_ket)
+#for i in range(1000):
+#    overlap_pp_block(A,B,alpha_bra,alpha_ket)
