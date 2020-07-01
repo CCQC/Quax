cffb7af515fe10bc070a02a602c55af74e69f6da
adabbott <adabbott@uga.edu>
2020-07-01 15:49:59 -0400

    cleaned up basis utils

--- psijax/master/integrals/basis_utils.py
+++ psijax/master/integrals/basis_utils.py
@@ -14,14 +14,11 @@ def build_basis_set(molecule, basis):
     basis_set = psi4.core.BasisSet.build(molecule, 'BASIS', basis, puream=0)
     # Get total number of shells for the molecule
     nshell = basis_set.nshell()
-    
     # Loop over each shell
     for i in range(nshell):
         # Create subdictionary for shell i that contains angular momentum
         # and coefficient/exponent information for each primitive
         basis_dict[i] = {}
-        #basis_dict[i]['am'] = basis_set.shell(i).amchar
-        #basis_dict[i]['am'] = str(basis_set.shell(i).am)
         basis_dict[i]['am'] = basis_set.shell(i).am
         basis_dict[i]['atom'] = basis_set.shell_to_center(i)
         basis_dict[i]['exp'] = []
@@ -35,28 +32,7 @@ def build_basis_set(molecule, basis):
             # Save the exponent and normalized coefficient of each primitive
             basis_dict[i]['exp'].append(basis_set.shell(i).exp(j))
             basis_dict[i]['coef'].append(basis_set.shell(i).coef(j))
-        #basis_dict[i]['exp'] = np.asarray(basis_dict[i]['exp'])
-        #basis_dict[i]['coef'] = np.asarray(basis_dict[i]['coef'])
     return basis_dict
-
-def homogenize_basisdict(basis_dict, max_prim):
-    '''
-    Make it so all contractions are the same size in the basis dict by padding exp and coef values 
-    to 1 and 0. Also create 'indices' key which says where along axis the integral should go, 
-    but padded with -1's to maximum angular momentum size
-    This allows you to pack them neatly into an array, and then worry about redundant computation later
-    '''
-    new_dict = basis_dict.copy()
-    for i in range(len(basis_dict)):
-        current_exp = onp.asarray(basis_dict[i]['exp'])
-        new_dict[i]['exp'] = onp.asarray(onp.pad(current_exp, (0, max_prim - current_exp.shape[0]), constant_values=1))
-        current_coef = onp.asarray(basis_dict[i]['coef'])
-        new_dict[i]['coef'] = onp.asarray(onp.pad(current_coef, (0, max_prim - current_coef.shape[0])))
-        #TODO what to do about indices?
-        #idx, size = basis_dict[i]['idx'], basis_dict[i]['idx_stride']
-        #indices = onp.repeat(idx, size) + onp.arange(size)
-        #print(indices)
-    return new_dict
 
 # Example use
 #molecule = psi4.geometry("""
@@ -75,6 +51,5 @@ def homogenize_basisdict(basis_dict, max_prim):
 #
 #basis_set = psi4.core.BasisSet.build(molecule, 'BASIS', basis, puream=0)
 
-        
 #basis_dict = build_basis_set(molecule,basis)
 #pprint.pprint(basis_dict)
