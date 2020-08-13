8a49403fd1756cf3472fe5d0db329c887f2a765e
adabbott <adabbott@uga.edu>
2020-08-13 18:11:13 -0400

    WIP, optimizing oei code the same way i optimized tei code.

--- PsiJax/psijax/psijax/integrals/oei.py
+++ PsiJax/psijax/psijax/integrals/oei.py
@@ -5,24 +5,30 @@ import jax.numpy as np
 from jax.experimental import loops
 from functools import partial
 
-from .integrals_utils import gaussian_product, boys, binomial_prefactor, factorials, double_factorials, cartesian_product, am_leading_indices, angular_momentum_combinations
+from .integrals_utils import gaussian_product, boys, binomial_prefactor, new_binomial_prefactor, factorials, double_factorials, cartesian_product, am_leading_indices, angular_momentum_combinations
 from .basis_utils import flatten_basis_data, get_nbf
 
-def overlap(aa,La,A,bb,Lb,B):
+def overlap(aa,La,bb,Lb,PA,PB,prefactor):
     """
     Computes a single overlap integral. Taketa, Hunzinaga, Oohata 2.12
+    P = gaussian product of aa,A; bb,B
+    PA = P - A
+    PB = P - B
+    PA_pow, PB_pow
+        All powers of Pi-Ai or Pi-Bi packed into an array
+        [[(Px-Ax)^0, (Px-Ax)^1, ... (Px-Ax)^max_am]
+         [(Py-Ay)^0, (Py-Ay)^1, ... (Py-Ay)^max_am]
+         [(Pz-Az)^0, (Pz-Az)^1, ... (Pz-Az)^max_am]]
+    prefactor = np.exp(-aa * bb * np.dot(A-B,A-B) / gamma)
     """
     la,ma,na = La 
     lb,mb,nb = Lb 
-    rab2 = np.dot(A-B,A-B)
     gamma = aa + bb
-    P = gaussian_product(aa,A,bb,B)
+    prefactor *= (np.pi / gamma)**1.5
 
-    prefactor = (np.pi / gamma)**1.5 * np.exp(-aa * bb * rab2 / gamma)
-
-    wx = overlap_component(la,lb,P[0]-A[0],P[0]-B[0],gamma)
-    wy = overlap_component(ma,mb,P[1]-A[1],P[1]-B[1],gamma)
-    wz = overlap_component(na,nb,P[2]-A[2],P[2]-B[2],gamma)
+    wx = overlap_component(la,lb,PA[0],PB[0],gamma)
+    wy = overlap_component(ma,mb,PA[1],PB[1],gamma)
+    wz = overlap_component(na,nb,PA[2],PB[2],gamma)
     return prefactor * wx * wy * wz
 
 def overlap_component(l1,l2,PAx,PBx,gamma):
@@ -34,25 +40,33 @@ def overlap_component(l1,l2,PAx,PBx,gamma):
       s.total = 0.
       s.i = 0
       for _ in s.while_range(lambda: s.i < K):
-        s.total += binomial_prefactor(2*s.i,l1,l2,PAx,PBx) * double_factorials[2*s.i-1] / (2*gamma)**s.i
+        #s.total += binomial_prefactor(2*s.i,l1,l2,PAx,PBx) * double_factorials[2*s.i-1] / (2*gamma)**s.i
+        #TODO create 2*gamma powers array #worthit
+        s.total += new_binomial_prefactor(2*s.i,l1,l2,PAx,PBx) * double_factorials[2*s.i-1] / (2*gamma)**s.i
         s.i += 1
       return s.total
 
-def kinetic(aa,La,A,bb,Lb,B):
+def kinetic(aa,La,bb,Lb,PA,PB,prefactor):
     """
     Computes a single kinetic energy integral.
     """
     la,ma,na = La
     lb,mb,nb = Lb
-    term1 = bb*(2*(lb+mb+nb)+3) * overlap(aa,La,A,bb,Lb,B)
+    term1 = bb*(2*(lb+mb+nb)+3) * overlap(aa,La,bb,Lb,PA,PB,prefactor)
 
-    term2 = -2 * bb**2 * (overlap(aa,(la,ma,na), A, bb,(lb+2,mb,nb),B) \
-                        + overlap(aa,(la,ma,na),A,bb,(lb,mb+2,nb),B) \
-                        + overlap(aa,(la,ma,na),A,bb,(lb,mb,nb+2),B))
+    #TODO currently kinetic calls overlap_component 21 times, but can intead just call 9 times:
+    # wx,wy,wz, wx_p2, wy_p2, wz_p2, wx_m2, wy_m2, wz_m2
 
-    term3 = -0.5 * (lb * (lb-1) * overlap(aa,(la,ma,na),A,bb,(lb-2,mb,nb),B) \
-                  + mb * (mb-1) * overlap(aa,(la,ma,na),A,bb,(lb,mb-2,nb),B) \
-                  + nb* (nb-1) * overlap(aa,(la,ma,na),A,bb,(lb,mb,nb-2),B))
+    #TODO these overlap calls repeat work on wx, wy, wz components. Should rewrite this.
+    # 1st call only alters wx. 2nd call only alters wy. 3rd call only alters wz
+    term2 = -2 * bb**2 * (overlap(aa,(la,ma,na), bb,(lb+2,mb,nb),PA,PB,prefactor) \
+                        + overlap(aa,(la,ma,na), bb,(lb,mb+2,nb),PA,PB,prefactor) \
+                        + overlap(aa,(la,ma,na), bb,(lb,mb,nb+2),PA,PB,prefactor))
+
+    # 1st call only alters wx. 2nd call only alters wy. 3rd call only alters wz
+    term3 = -0.5 * (lb * (lb-1) * overlap(aa,(la,ma,na),bb,(lb-2,mb,nb),PA,PB,prefactor) \
+                  + mb * (mb-1) * overlap(aa,(la,ma,na),bb,(lb,mb-2,nb),PA,PB,prefactor) \
+                  + nb * (nb-1) * overlap(aa,(la,ma,na),bb,(lb,mb,nb-2),PA,PB,prefactor))
     return term1 + term2 + term3
 
 def A_term(i,r,u,l1,l2,PAx,PBx,CPx,gamma):
@@ -85,31 +99,27 @@ def A_array(l1,l2,PA,PB,CP,g):
         s.i -= 1
       return s.A
 
-def potential(aa, La, A, bb, Lb, B, geom, charges):
+def potential(aa, La, bb, Lb, PA, PB, P, prefactor, geom, charges):
     """
     Computes a single electron-nuclear attraction integral
     """
     la,ma,na = La
     lb,mb,nb = Lb
     gamma = aa + bb
-    P = gaussian_product(aa,A,bb,B)
-    rab2 = np.dot(A-B,A-B)
+    prefactor *= -2 * np.pi / gamma
 
     with loops.Scope() as s:
       s.val = 0.
       for i in s.range(geom.shape[0]):
         C = geom[i]
         rcp2 = np.dot(C-P,C-P)
-        dPA = P-A
-        dPB = P-B
-        dPC = P-C
+        PC = P-C
 
-        Ax = A_array(la,lb,dPA[0],dPB[0],dPC[0],gamma)
-        Ay = A_array(ma,mb,dPA[1],dPB[1],dPC[1],gamma)
-        Az = A_array(na,nb,dPA[2],dPB[2],dPC[2],gamma)
+        Ax = A_array(la,lb,PA[0],PB[0],PC[0],gamma)
+        Ay = A_array(ma,mb,PA[1],PB[1],PC[1],gamma)
+        Az = A_array(na,nb,PA[2],PB[2],PC[2],gamma)
 
         boys_arg = gamma * rcp2
-
 
         with loops.Scope() as S:
           S.total = 0.
@@ -121,11 +131,12 @@ def potential(aa, La, A, bb, Lb, B, geom, charges):
             for _ in S.while_range(lambda: S.J < ma + mb + 1):
               S.K = 0 
               for _ in S.while_range(lambda: S.K < na + nb + 1):
+                #TODO pull out boys function
                 S.total += Ax[S.I] * Ay[S.J] * Az[S.K] * boys(S.I + S.J + S.K, boys_arg)
                 S.K += 1
               S.J += 1
             S.I += 1
-        s.val += charges[i] * -2 * np.pi / gamma * np.exp(-aa * bb * rab2 / gamma) * S.total
+        s.val += charges[i] * prefactor * S.total
       return s.val
 
 def oei_arrays(geom, basis, charges):
@@ -155,8 +166,18 @@ def oei_arrays(geom, basis, charges):
         A, B = geom[atom1], geom[atom2]
         am1, am2 = ams[p1], ams[p2]
         ld1, ld2 = am_leading_indices[am1], am_leading_indices[am2]
-        xa,ya,za = A 
-        xb,yb,zb = B 
+
+        gamma = aa + bb
+        prefactor = np.exp(-aa * bb * np.dot(A-B,A-B) / gamma)
+        P = (aa * A + bb * B) / gamma
+        PA = P - A
+        PB = P - B
+        # Maximum angular momentum: hard coded
+        max_am = 3 # f function support
+        # Precompute all powers of Pi-Ai, Pi-Bi
+        PA_pow = np.power(np.broadcast_to(P-A, (max_am+1,3)).T, np.arange(max_am+1))
+        PB_pow = np.power(np.broadcast_to(P-B, (max_am+1,3)).T, np.arange(max_am+1))
+
 
         s.a = 0
         for _ in s.while_range(lambda: s.a < dims[p1]):
@@ -168,9 +189,12 @@ def oei_arrays(geom, basis, charges):
             i = indices[p1] + s.a
             j = indices[p2] + s.b
             # Compute one electron integrals and add to appropriate index
-            overlap_int = overlap(aa,La,A,bb,Lb,B) * coef
-            kinetic_int = kinetic(aa,La,A,bb,Lb,B) * coef
-            potential_int = potential(aa,La,A,bb,Lb,B,geom,charges) * coef
+            #overlap_int = overlap(aa,La,bb,Lb,PA,PB,prefactor) * coef
+            #kinetic_int = kinetic(aa,La,bb,Lb,PA,PB,prefactor) * coef
+            overlap_int = overlap(aa,La,bb,Lb,PA_pow,PB_pow,prefactor) * coef
+            kinetic_int = kinetic(aa,La,bb,Lb,PA_pow,PB_pow,prefactor) * coef
+            potential_int = potential(aa,La,bb,Lb,PA,PB,P,prefactor,geom,charges) * coef
+
             s.S = jax.ops.index_add(s.S, jax.ops.index[i,j], overlap_int) 
             s.T = jax.ops.index_add(s.T, jax.ops.index[i,j], kinetic_int) 
             s.V = jax.ops.index_add(s.V, jax.ops.index[i,j], potential_int) 
@@ -180,11 +204,12 @@ def oei_arrays(geom, basis, charges):
 
 #import psi4
 #import numpy as onp
-#from basis_utils import build_basis_set
+#from basis_utils import build_basis_set,flatten_basis_data,get_nbf
+#from integrals_utils import gaussian_product, boys, new_binomial_prefactor, binomial_prefactor, factorials, double_factorials, cartesian_product, am_leading_indices, angular_momentum_combinations
 #molecule = psi4.geometry("""
 #                         0 1
-#                         C 0.0 0.0 -0.849220457955
-#                         O 0.0 0.0  0.849220457955
+#                         H 0.0 0.0 -0.849220457955
+#                         H 0.0 0.0  0.849220457955
 #                         units bohr
 #                         """)
 #geom = np.asarray(onp.asarray(molecule.geometry()))
