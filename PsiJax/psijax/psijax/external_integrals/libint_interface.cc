b9b824ef5a8d4b8f4d9f3b662ce4e8d25e766dae
adabbott <adabbott@uga.edu>
2020-10-26 15:48:06 -0400

    currently return python array of overlap, lots of comments about to be removed, checkpoint.

--- PsiJax/psijax/psijax/external_integrals/libint_interface.cc
+++ PsiJax/psijax/psijax/external_integrals/libint_interface.cc
@@ -21,30 +21,26 @@ int add(int i, int j) {
 }
 
 // Function takes two strings: xyzfile absolute path (TODO in Angstroms) and basis set name  
-// Builds BasisSet objects, which are basically vectors of Shell's.
-// Prints the basis set info
-int test(std::string xyzfilename, std::string basis_name) {
+// Then computes overlap integrals
+//py::array_t<double> test(std::string xyzfilename, std::string basis_name) {
+py::array overlap(std::string xyzfilename, std::string basis_name) {
     using namespace libint2;
     using namespace std;
     libint2::initialize();
 
-    // TODO assumes units are angstroms.. uh oh
-    // if bohr:
-    //   conversion = const double 1.0
-    //   vector<Atom> atoms = read_dotxyz(input_file, conversion)
+    // Load basis set and geometry. TODO this assumes units are angstroms... 
     ifstream input_file(xyzfilename);
     vector<Atom> atoms = read_dotxyz(input_file);
     BasisSet obs(basis_name, atoms);
     obs.set_pure(false); // use cartesian gaussians
 
-    // Test printint basis set info
-    std::copy(begin(obs), end(obs),
-          std::ostream_iterator<Shell>(std::cout, "\n"));
 
     //Engine eri_engine(Operator::coulomb, obs.max_nprim(), obs.max_l());
-
-    // Compute overlap integrals This fails because i did not initialize... hmmm
+    // Overlap integrals engine 
     Engine s_engine(Operator::overlap,obs.max_nprim(),obs.max_l());
+
+    size_t length = obs.size() * obs.size();
+    std::vector<double> result(length);
 
     auto shell2bf = obs.shell2bf(); // maps shell index to basis function index
     const auto& buf_vec = s_engine.results(); // will point to computed shell sets
@@ -52,9 +48,9 @@ int test(std::string xyzfilename, std::string basis_name) {
     for(auto s1=0; s1!=obs.size(); ++s1) {
       for(auto s2=0; s2!=obs.size(); ++s2) {
     
-        cout << "compute shell set {" << s1 << "," << s2 << "} ... ";
+        //cout << "compute shell set {" << s1 << "," << s2 << "} ... ";
         s_engine.compute(obs[s1], obs[s2]);
-        cout << "done" << endl;
+        //cout << "done" << endl;
         auto ints_shellset = buf_vec[0];  // location of the computed integrals
         if (ints_shellset == nullptr)
           continue;  // nullptr returned if the entire shell-set was screened out
@@ -69,13 +65,17 @@ int test(std::string xyzfilename, std::string basis_name) {
         // TODO how to pack ints_shellset into final vector representing entire array?
         for(auto f1=0; f1!=n1; ++f1)
           for(auto f2=0; f2!=n2; ++f2)
-            cout << "  " << bf1+f1 << " " << bf2+f2 << " " << ints_shellset[f1*n2+f2] << endl;
+            //result[f1 * n2 + f2]; // dummy add
+            //result[f1*n2+f2] = ints_shellset[f1 * n2 + f2];
+            // true idx is probably  s1 * n2 + s2 + f1 * n2 + f2
+            result[s1 * n2 + s2 + f1 * n2 + f2] = ints_shellset[f1 * n2 + f2];
+            //cout << "  " << bf1+f1 << " " << bf2+f2 << " " << ints_shellset[f1*n2+f2] << endl;
       }
     }
-
-
     libint2::finalize();
-    return 0;
+
+    return py::array(result.size(), result.data()); // This apparently copies data, but it should be fine right? https://github.com/pybind/pybind11/issues/1042
+    //return 0;
 }
 
 // Define module named 'libint_interface' which can be imported with python
@@ -84,13 +84,21 @@ int test(std::string xyzfilename, std::string basis_name) {
 PYBIND11_MODULE(libint_interface, m) {
     m.doc() = "pybind11 libint interface to molecular integrals"; // optional module docstring
     m.def("add", &add, "A function which adds two numbers");
-    m.def("test", &test, "Dummy function which does libint2 stuff and returns 0");
+    m.def("overlap", &overlap, "Computes overlap integrals with libint");
 }
 
 // Temporary libint reference: new shared library compilation
 // currently needs export LD_LIBRARY_PATH=/path/to/libint2.so
+// Compilation script for libint with am=2 deriv=4, may need to set LD_LIBRARY_PATH = /path/to/libint2.so corresponding to this installation.
+// g++ libint_interface.cc -o libint_interface`python3-config --extension-suffix`  -O3 -fPIC -shared -std=c++11 -I/home/adabbott/anaconda3/envs/psijax/include/python3.6m -I/home/adabbott/anaconda3/envs/psijax/lib/python3.6/site-packages/pybind11/include -I/home/adabbott/anaconda3/envs/psijax/include/eigen3 -I/home/adabbott/Git/libint/BUILD/libint-2.7.0-beta.6/include/libint2 -I/home/adabbott/Git/libint/BUILD/libint-2.7.0-beta.6/include -L/home/adabbott/Git/libint/BUILD/libint-2.7.0-beta.6/ -lint2 
 
-// New compile with full libint am=2 deriv=4 
+// Warning: above is very slow since its a huge copy of libint. can use smaller version, just s, p with gradients,
+// Can do quick compile with the following:
+//g++ -c libint_interface.cc -o libint_interface.o -O3 -fPIC -shared -std=c++11 -I/home/adabbott/anaconda3/envs/psijax/include/python3.6m -I/home/adabbott/anaconda3/envs/psijax/lib/python3.6/site-packages/pybind11/include -I/home/adabbott/anaconda3/envs/psijax/include/eigen3 -I/home/adabbott/Git/libint_pgrad/libint-2.7.0-beta.6/PREFIX/include -I/home/adabbott/Git/libint_pgrad/libint-2.7.0-beta.6/PREFIX/include/libint2 -lint2 -L/home/adabbott/Git/libint_pgrad/libint-2.7.0-beta.6/PREFIX/lib
+
+//g++ libint_interface.o -o libint_interface`python3-config --extension-suffix` -O3 -fPIC -shared -std=c++11 -I/home/adabbott/anaconda3/envs/psijax/include/python3.6m -I/home/adabbott/anaconda3/envs/psijax/lib/python3.6/site-packages/pybind11/include -I/home/adabbott/anaconda3/envs/psijax/include/eigen3 -I/home/adabbott/Git/libint_pgrad/libint-2.7.0-beta.6/PREFIX/include -I/home/adabbott/Git/libint_pgrad/libint-2.7.0-beta.6/PREFIX/include/libint2 -lint2 -L/home/adabbott/Git/libint_pgrad/libint-2.7.0-beta.6/PREFIX/lib
+
+
 //g++ -c libint_interface.cc -o libint_interface.o -O3 -fPIC -shared -std=c++11 -I/home/adabbott/anaconda3/envs/psijax/include/python3.6m -I/home/adabbott/anaconda3/envs/psijax/lib/python3.6/site-packages/pybind11/include -I/home/adabbott/anaconda3/envs/psijax/include/eigen3 -I/home/adabbott/Git/libint/BUILD/libint-2.7.0-beta.6/include/libint2 -I/home/adabbott/Git/libint/BUILD/libint-2.7.0-beta.6/include -L/home/adabbott/Git/libint/BUILD/libint-2.7.0-beta.6/ -lint2 
 
 //g++ libint_interface.o -o libint_interface`python3-config --extension-suffix`  -O3 -fPIC -shared -std=c++11 -I/home/adabbott/anaconda3/envs/psijax/include/python3.6m -I/home/adabbott/anaconda3/envs/psijax/lib/python3.6/site-packages/pybind11/include -I/home/adabbott/anaconda3/envs/psijax/include/eigen3 -I/home/adabbott/Git/libint/BUILD/libint-2.7.0-beta.6/include/libint2 -I/home/adabbott/Git/libint/BUILD/libint-2.7.0-beta.6/include -lint2 -L/home/adabbott/Git/libint/BUILD/libint-2.7.0-beta.6
@@ -98,7 +106,15 @@ PYBIND11_MODULE(libint_interface, m) {
 // TODO let's try to simplify the above... can we combine into one compile?
 //g++ -O3 -fPIC -shared -std=c++11 -I/home/adabbott/anaconda3/envs/psijax/include/python3.6m -I/home/adabbott/anaconda3/envs/psijax/lib/python3.6/site-packages/pybind11/include -I/home/adabbott/anaconda3/envs/psijax/include/eigen3 -I/home/adabbott/Git/libint/BUILD/libint-2.7.0-beta.6/include/libint2 -I/home/adabbott/Git/libint/BUILD/libint-2.7.0-beta.6/include -L/home/adabbott/Git/libint/BUILD/libint-2.7.0-beta.6/ -lint2 libint_interface.cc -o libint_interface`python3-config --extension-suffix` 
 
+// Okay it fails with the follwoing:
+// /home/adabbott/Git/PsiTorch/PsiJax/psijax/psijax/external_integrals/libint_interface.cpython-36m-x86_64-linux-gnu.so: undefined symbol: libint2_build_default
 
+// Does it work if i cll script first?
+//g++ libint_interface.cc -o libint_interface`python3-config --extension-suffix`  -O3 -fPIC -shared -std=c++11 -I/home/adabbott/anaconda3/envs/psijax/include/python3.6m -I/home/adabbott/anaconda3/envs/psijax/lib/python3.6/site-packages/pybind11/include -I/home/adabbott/anaconda3/envs/psijax/include/eigen3 -I/home/adabbott/Git/libint/BUILD/libint-2.7.0-beta.6/include/libint2 -I/home/adabbott/Git/libint/BUILD/libint-2.7.0-beta.6/include -L/home/adabbott/Git/libint/BUILD/libint-2.7.0-beta.6/ -lint2 
+
+// Yes the above works just fine... why do things work fine when I put the .cc first?
+
+//g++ libint_interface.cc -o libint_interface`python3-config --extension-suffix`  -O3 -fPIC -shared -std=c++11 -I/home/adabbott/anaconda3/envs/psijax/include/python3.6m -I/home/adabbott/anaconda3/envs/psijax/lib/python3.6/site-packages/pybind11/include -I/home/adabbott/anaconda3/envs/psijax/include/eigen3 
 
 
 // Then two stage compile:
