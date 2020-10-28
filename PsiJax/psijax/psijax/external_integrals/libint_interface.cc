f53be1f585b93a55f48eafaa788d1215d567e3d3
adabbott <adabbott@uga.edu>
2020-10-28 13:10:19 -0400

    about to revert last algo

--- PsiJax/psijax/psijax/external_integrals/libint_interface.cc
+++ PsiJax/psijax/psijax/external_integrals/libint_interface.cc
@@ -1,6 +1,7 @@
 #include <omp.h>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <stdlib.h>
 #include <libint2.hpp>
 
@@ -213,6 +214,139 @@ py::array eri(std::string xyzfilename, std::string basis_name) {
 }
 
 
+// Coulomb integral derivative function
+py::array eri_deriv(std::string xyzfilename, std::string basis_name, std::vector<int> deriv_vec) {
+    using namespace libint2;
+    using namespace std;
+    libint2::initialize();
+
+    // Get order of differentiation
+    int deriv_order = accumulate(deriv_vec.begin(), deriv_vec.end(), 0);
+    // Number of derivatives at this order of differentiation. 
+    // 1/n! * Multiply(from i=0 to n-1)(3k + i) where n = deriv_order and k=4 number of centers
+    std::vector<int> total_shell_derivatives{0, 12, 78, 364, 1365};
+    int num_derivs = total_shell_derivatives[deriv_order];
+
+    // Load basis set and geometry. TODO this assumes units are angstroms... 
+    ifstream input_file(xyzfilename);
+    vector<Atom> atoms = read_dotxyz(input_file);
+    BasisSet obs(basis_name, atoms);
+    obs.set_pure(false); // use cartesian gaussians
+    int nbf = obs.nbf();
+    // ERI derivative integral engine
+    Engine eri_engine(Operator::coulomb,obs.max_nprim(),obs.max_l(),deriv_order);
+
+    auto natom = atoms.size();
+    std::vector<bool> desired_atoms(natom, false);
+    // Which atoms are involved in the deriv_vec?
+    for (int i = 0; i < natom; i++) {
+        bool desired = false;
+        // x
+        if (deriv_vec[3 * i + 0] > 0) desired = true;
+        // y
+        if (deriv_vec[3 * i + 1] > 0) desired = true;
+        // z
+        if (deriv_vec[3 * i + 2] > 0) desired = true;
+        desired_atoms[i] = desired;
+    }
+
+    // Get size of ERI array and allocate 
+    size_t length = nbf * nbf * nbf * nbf;
+    std::vector<double> result(length);
+
+    auto shell2bf = obs.shell2bf(); // maps shell index to basis function index
+    const auto shell2atom = obs.shell2atom(atoms); // maps shell index to atom index
+    const auto& buf_vec = eri_engine.results(); // will point to computed shell sets
+    
+    for(auto s1=0; s1!=obs.size(); ++s1) {
+        auto bf1 = shell2bf[s1];     // Index of first basis function in shell 1
+        auto atom1 = shell2atom[s1]; // Atom index of shell 1
+        auto n1 = obs[s1].size();    // number of basis functions in shell 1
+        for(auto s2=0; s2!=obs.size(); ++s2) {
+            auto bf2 = shell2bf[s2];     // Index of first basis function in shell 2
+            auto atom2 = shell2atom[s2]; // Atom index of shell 2
+            auto n2 = obs[s2].size();    // number of basis functions in shell 2
+            for(auto s3=0; s3!=obs.size(); ++s3) {
+                auto bf3 = shell2bf[s3];     // Index of first basis function in shell 3
+                auto atom3 = shell2atom[s3]; // Atom index of shell 3
+                auto n3 = obs[s3].size();    // number of basis functions in shell 3
+                for(auto s4=0; s4!=obs.size(); ++s4) {
+                    auto bf4 = shell2bf[s4];     // Index of first basis function in shell 4
+                    auto atom4 = shell2atom[s4]; // Atom index of shell 4
+                    auto n4 = obs[s4].size();    // number of basis functions in shell 4
+
+                    // If the atoms are the same we ignore it as the derivatives will be zero.
+                    if (atom1 == atom2 && atom1 == atom3 && atom1 == atom4) continue;
+
+                    // Create list of atom indices corresponding to each shell 
+                    std::vector<int> atom_index_list{atom1,atom2,atom3,atom4};
+
+                    // Check if EVERY differentiated atom according to deriv_vec is in atom_index_list, if not, skip computing this shell quartet derivative
+                    // Screens out a lot of higher order derivatives, or if you have a lot of atoms.
+                    bool tmp = true;
+                    for (int i = 0; i < natom; ++i) {
+                        if (desired_atoms[i]) {
+                            tmp = (tmp && (i == atom1 || i == atom2 || i == atom3 || i == atom4));
+                        }
+                    }
+                    if (tmp == false) continue;
+
+                    // If we made it this far, the shell derivative we want is in the buffer, perhaps even more than once.
+                    // Compute this shell quartet derivative, and find one of the buffer indices which gives it to us.
+                    eri_engine.compute(obs[s1], obs[s2], obs[s3], obs[s4]); // Compute shell set, fills buf_vec
+
+                    // The buf_vec contains two indices: first index is which shell quartet derivative operator, second is shell set
+                    // We need to loop over first index, deriv_index, and check if it is an index which corresponds to deriv_vec, 
+                    // which is in terms of nuclear derivatives, distinct from shell derivatives
+                    for (int deriv_index = 0; deriv_index < num_derivs; ++deriv_index) {
+                        // Map this shell derivative index, e.g. d^2/ dAx dCx to its corresponding nuclear derivative index in flattened cartesian geometry array
+                        // TODO define cart_component_indices, shell_center_indices
+                        cart_indices = cart_component_indices[deriv_index];      // Length = deriv_order, integer says which cartesian component
+                        shell_deriv_indices = shell_center_indices[deriv_index]; // Length = deriv_order, integer 0,1,2,3 says which shell is involved in each partial derivative
+                    
+                        // Grab each cartesian coordinate index in (nbf,nbf,nbf,nbf,g1,g2,g3,...)  
+                        //  slice (nbf,nbf,nbf,nbf,1,1,1...)
+                        std::vector<int> coord_indices(deriv_order);
+                        for (int i = 0, i < deriv_order, ++i) {
+                            int atom_idx = atom_index_list[shell_deriv_indices[i]]; // atom index this shell derivative corresponds to
+                            int cart_component = cart_indices[i];                   // cartesian component 0,1,2-->x,y,z
+                            int coord_idx = 3 * atom_idx + cart_component;          // index in flattened cartesian coordinate geometry
+                            coord_indices.push_back(coord_idx);
+                        // Now we check if these are the cartesian coordinates we want in deriv_vec
+                        // e.g. if deriv_vec is [2,1,0,0,0...,0] then we have coord_indices = [0,0,1]
+                         
+                        }
+                    }
+
+                    //TEMP TODO try other integers
+                    auto ints_shellset = buf_vec[1];                        // Location of the computed integrals
+
+                    if (ints_shellset == nullptr)
+                        continue;  // nullptr returned if the entire shell-set was screened out
+    
+                    // Loop over shell block, keeping a total count idx for the size of shell set
+                    for(auto f1=0, idx=0; f1!=n1; ++f1) {
+                        size_t offset_1 = (bf1 + f1) * nbf * nbf * nbf;
+                        for(auto f2=0; f2!=n2; ++f2) {
+                            size_t offset_2 = (bf2 + f2) * nbf * nbf;
+                            for(auto f3=0; f3!=n3; ++f3) {
+                                size_t offset_3 = (bf3 + f3) * nbf;
+                                for(auto f4=0; f4!=n4; ++f4, ++idx) {
+                                    result[offset_1 + offset_2 + offset_3 + bf4 + f4] = ints_shellset[idx];
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    libint2::finalize();
+    return py::array(result.size(), result.data()); // This apparently copies data, but it should be fine right? https://github.com/pybind/pybind11/issues/1042 there's a workaround
+}
+
+
+
 
 // Define module named 'libint_interface' which can be imported with python
 // The second arg, 'm' defines a variable py::module_ which can be used to create
@@ -226,7 +360,7 @@ PYBIND11_MODULE(libint_interface, m) {
     //m.def("overlap_deriv", &overlap_deriv, "Computes overlap integral nuclear derivatives with libint");
     //m.def("kinetic_deriv", &kinetic_deriv, "Computes kinetic integral nuclear derivatives with libint");
     //m.def("potential_deriv", &potential_deriv, "Computes potential integral nuclear derivatives with libint");
-    //m.def("eri_deriv", &eri_deriv, "Computes electron repulsion integral nuclear derivatives with libint");
+    m.def("eri_deriv", &eri_deriv, "Computes electron repulsion integral nuclear derivatives with libint");
 
 }
 
