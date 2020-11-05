b16b927afc1216b597267c03e705185b584ec738
adabbott <adabbott@uga.edu>
2020-11-05 14:31:42 -0500

    updated makefile to new libint install. integral test suite for overlap, kinetic, ERIs working up through 4th order (quartic). Potentials working through hessians, Libint has not implemented beyond 2nd in Engine interface.

--- PsiJax/psijax/psijax/external_integrals/integral_test_suite.py
+++ PsiJax/psijax/psijax/external_integrals/integral_test_suite.py
@@ -31,6 +31,17 @@ basis_dict = build_basis_set(molecule, basis_name)
 charge = molecule.molecular_charge()
 nuclear_charges = np.asarray([molecule.charge(i) for i in range(geom.shape[0])])
 
+# Old integrals 
+def wrap_oeis(geomflat):
+    geom = geomflat.reshape(-1,3)
+    S, T, V = oei_arrays(geom,basis_dict,nuclear_charges)
+    return S, T, V
+
+# TEI's
+def wrap(geomflat):
+    geom = geomflat.reshape(-1,3)
+    return tei_array(geom, basis_dict) 
+
 # New libint interface gradients and hessians
 # Initialize Libint
 psijax.external_integrals.libint_initialize(xyz_path, basis_name)
@@ -43,11 +54,6 @@ new_overlap_hess = jax.jacfwd(jax.jacfwd(psijax.external_integrals.overlap))(geo
 new_kinetic_hess = jax.jacfwd(jax.jacfwd(psijax.external_integrals.kinetic))(geomflat)
 new_potential_hess = jax.jacfwd(jax.jacfwd(psijax.external_integrals.potential))(geomflat)
 
-# Old integrals 
-def wrap_oeis(geomflat):
-    geom = geomflat.reshape(-1,3)
-    S, T, V = oei_arrays(geom,basis_dict,nuclear_charges)
-    return S, T, V
 overlap_grad, kinetic_grad, potential_grad = jax.jacfwd(wrap_oeis)(geomflat)
 
 print("overlap gradients match ", onp.allclose(overlap_grad, new_overlap_grad))
@@ -60,10 +66,6 @@ print("overlap hessians match ", onp.allclose(overlap_hess, new_overlap_hess))
 print("kinetic hessians match ", onp.allclose(kinetic_hess, new_kinetic_hess))
 print("potential hessians match ", onp.allclose(potential_hess, new_potential_hess))
 
-# TEI's
-def wrap(geomflat):
-    geom = geomflat.reshape(-1,3)
-    return tei_array(geom, basis_dict) 
 
 new_tei_grad = jax.jacfwd(psijax.external_integrals.tei)(geomflat)
 new_tei_hess = jax.jacfwd(jax.jacfwd(psijax.external_integrals.tei))(geomflat)
@@ -74,35 +76,37 @@ tei_hess = jax.jacfwd(jax.jacfwd(wrap))(geomflat)
 print("ERI hessians match ", onp.allclose(tei_hess, new_tei_hess))
 
 # Cubics 
+# OKAY LIBINT ENGINE DOES NOT SUPPORT POTENTIALS
 # New code
 new_overlap_cube = jax.jacfwd(jax.jacfwd(jax.jacfwd(psijax.external_integrals.overlap)))(geomflat)
 new_kinetic_cube = jax.jacfwd(jax.jacfwd(jax.jacfwd(psijax.external_integrals.kinetic)))(geomflat)
-new_potential_cube = jax.jacfwd(jax.jacfwd(jax.jacfwd(psijax.external_integrals.potential)))(geomflat)
+#new_potential_cube = jax.jacfwd(jax.jacfwd(jax.jacfwd(psijax.external_integrals.potential)))(geomflat)
 new_tei_cube = jax.jacfwd(jax.jacfwd(jax.jacfwd(psijax.external_integrals.tei)))(geomflat)
+print(new_tei_cube.shape)
 
-# Old code
+## Old code
 overlap_cube, kinetic_cube, potential_cube = jax.jacfwd(jax.jacfwd(jax.jacfwd(wrap_oeis)))(geomflat)
 tei_cube = jax.jacfwd(jax.jacfwd(jax.jacfwd(wrap)))(geomflat)
 
 print("overlap cubics match ", onp.allclose(overlap_cube, new_overlap_cube))
 print("kinetic cubics match ", onp.allclose(kinetic_cube, new_kinetic_cube))
-print("potential cubics match ", onp.allclose(potential_cube, new_potential_cube))
+#print("potential cubics match ", onp.allclose(potential_cube, new_potential_cube))
 print("ERI cubics match ", onp.allclose(tei_cube, new_tei_cube))
-
-# Quartics 
-# New code
+#
+## Quartics 
+## New code
 new_overlap_quartic = jax.jacfwd(jax.jacfwd(jax.jacfwd(jax.jacfwd(psijax.external_integrals.overlap))))(geomflat)
 new_kinetic_quartic = jax.jacfwd(jax.jacfwd(jax.jacfwd(jax.jacfwd(psijax.external_integrals.kinetic))))(geomflat)
-new_potential_quartic = jax.jacfwd(jax.jacfwd(jax.jacfwd(jax.jacfwd(psijax.external_integrals.potential))))(geomflat)
+#new_potential_quartic = jax.jacfwd(jax.jacfwd(jax.jacfwd(jax.jacfwd(psijax.external_integrals.potential))))(geomflat)
 new_tei_quartic = jax.jacfwd(jax.jacfwd(jax.jacfwd(jax.jacfwd(psijax.external_integrals.tei))))(geomflat)
 
-# Old code
+## Old code
 overlap_quartic, kinetic_quartic, potential_quartic = jax.jacfwd(jax.jacfwd(jax.jacfwd(jax.jacfwd(wrap_oeis))))(geomflat)
 tei_quartic = jax.jacfwd(jax.jacfwd(jax.jacfwd(jax.jacfwd(wrap))))(geomflat)
 
 print("overlap quartics match ", onp.allclose(overlap_quartic, new_overlap_quartic))
 print("kinetic quartics match ", onp.allclose(kinetic_quartic, new_kinetic_quartic))
-print("potential quartics match ", onp.allclose(potential_quartic, new_potential_quartic))
+#print("potential quartics match ", onp.allclose(potential_quartic, new_potential_quartic))
 print("ERI quartics match ", onp.allclose(tei_quartic, new_tei_quartic))
 
 # Finalize Libint
