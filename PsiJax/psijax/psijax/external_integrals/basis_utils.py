207fc219883a51cd122db70d9602a4d9355c3356
adabbott <adabbott@uga.edu>
2020-12-15 11:23:47 -0500

    small change

--- PsiJax/psijax/psijax/external_integrals/basis_utils.py
+++ PsiJax/psijax/psijax/external_integrals/basis_utils.py
@@ -1,6 +1,8 @@
 import psi4 
+import jax
 import jax.numpy as jnp
 import numpy as np
+jax.config.update("jax_enable_x64", True)
 
 def build_basis_set(molecule, basis):
     # Avoids printing from psi4
